a6a9111344bb484f1035ecf66e34b1586531e9a9
aryan262 <aryan.nikolas@gmail.com>
2024-07-30 09:25:59 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,19 @@ import postRoutes from "./routes/post.routes.js"
 import notificationRoutes from "./routes/notification.routes.js"
 import connectDB from "./db/connectDB.js";
 import {v2 as cloudinary} from "cloudinary";
-const __dirname = path.resolve()
-const app = express();
 dotenv.config();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use(express.json({limit:"20mb"}))
 app.use(express.urlencoded({extended:true}))
+
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -30,7 +33,7 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
     connectDB();
