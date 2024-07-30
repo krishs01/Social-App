4cdcb424ea445e1ba86069941026a7bac4a7c028
aryan262 <aryan.nikolas@gmail.com>
2024-07-30 22:55:43 +0530

    Big Change Part 2

--- backend/api/server.js
+++ backend/api/server.js
@@ -37,7 +37,5 @@ if(process.env.NODE_ENV === "production"){
     });
 }
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+
+export  default app
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,9 @@
 import app from './api/server.js'
+import connectDB from './db/connectDB.js';
 
-export {app}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+})
