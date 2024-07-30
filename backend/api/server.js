5aedef8ce458c1122ce7fe18f3d5b59a4d5b6530
aryan262 <aryan.nikolas@gmail.com>
2024-07-30 22:51:34 +0530

    Its a big change

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from "dotenv"
 import path from "path"
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js"
-import postRoutes from "./routes/post.routes.js"
-import notificationRoutes from "./routes/notification.routes.js"
-import connectDB from "./db/connectDB.js";
+import authRoutes from "../routes/auth.routes.js";
+import userRoutes from "../routes/user.routes.js"
+import postRoutes from "../routes/post.routes.js"
+import notificationRoutes from "../routes/notification.routes.js"
+import connectDB from "../db/connectDB.js";
 import {v2 as cloudinary} from "cloudinary";
 dotenv.config();
 cloudinary.config({
