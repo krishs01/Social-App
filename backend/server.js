65deaba5d6931c5e8d4eca2f798da098e2534aec
aryan262 <aryan.nikolas@gmail.com>
2024-07-30 20:30:03 +0530

    Added home route

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
-
+app.get("/", (req, res)=>{
+    res.send("Hello from server");
+}
+)
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
