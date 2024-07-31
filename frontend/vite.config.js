cf677411c3c20c67b447eefe39c047d608bfc002
aryan262 <aryan.nikolas@gmail.com>
2024-07-31 08:09:15 +0530

    Final Commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://social-app-mxgj.vercel.app/",
+				target: "https://localhost:5000/",
 				changeOrigin: true,
 			},
 		},
